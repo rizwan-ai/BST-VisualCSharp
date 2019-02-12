071d321ff0f969535ab6f4841bc4d3da85ae6f94
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2019-02-12 23:52:58 +0500

    Add files via upload

---
+++ BST-Traversing.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using static System.Console;
+
+namespace BST_Traversing
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+    }
+    public class BST
+    {
+        private Node root, temp, current, parent;
+        public BST() => root = null;
+        // Insertion Operation in BST (Simple-Logic ... Not Recursively)
+        public void InsertNode(int value)
+        {
+            // Create node and assign to it ...
+            temp = new Node();
+            temp.data = value;
+            temp.left = temp.right = null;
+            if (root == null)
+            {
+                root = temp;
+                return;
+            }
+            // Go to proper position to insert node ...
+            current = root;
+            while (current != null)
+            {
+                if (value == current.data)
+                {
+                    WriteLine($"Value {value} is already exist");
+                    return;
+                }
+                if (value < current.data)
+                {
+                    parent = current;
+                    current = current.left;
+                }
+                else
+                {
+                    parent = current;
+                    current = current.right;
+                }
+            }
+
+            if (value < parent.data)
+                parent.left = temp;
+            else
+                parent.right = temp;
+        }
+        // Traversing Operation in BST (Recursively)
+        // 1) InOrder Traversal 
+        public void InOrder() => InOrd(root);
+        private void InOrd(Node r)
+        {
+            if (r == null) return;
+            InOrd(r.left);
+            Write($"{r.data} ");
+            InOrd(r.right);
+        }
+        // 2) PreOrder Traversal 
+        public void PreOrder() => PreOrd(root);
+        private void PreOrd(Node r)
+        {
+            if (r == null) return;
+            Write($"{r.data} ");
+            PreOrd(r.left);
+            PreOrd(r.right);
+        }
+        // 3) PostOrder Traversal 
+        public void PostOrder() => PostOrd(root);
+        private void PostOrd(Node r)
+        {
+            if (r == null) return;
+            PostOrd(r.left);
+            PostOrd(r.right);
+            Write($"{r.data} ");
+        }
+    }
+}
---
+++ Program-BST-Traversing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace BST_Traversing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST bst = new BST();
+
+            Write("Enter Total Values : ");
+            int length = ToInt32(ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Write($"Enter Value {i + 1} : ");
+                int val = ToInt32(ReadLine());
+                bst.InsertNode(val);
+            }
+
+            WriteLine("InOrder Traversal");
+            bst.InOrder();
+            WriteLine("\nPreOrder Traversal");
+            bst.PreOrder();
+            WriteLine("\nInOrder Traversal");
+            bst.PostOrder();
+
+
+            ReadKey();
+        }
+    }
+}
