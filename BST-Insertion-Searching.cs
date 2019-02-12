53b08f96f306eb185cd36b7fa190ceeac08075e6
Muhammad Rizwan <34979598+MRizwanSE@users.noreply.github.com>
2019-02-12 22:39:33 +0500

    Add files via upload

---
+++ BST-Insertion-Searching.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace BST_Operations
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+    }
+    public class BST
+    {
+        private Node root, temp, current, parent;
+        public BST() => root = null;
+        // Insertion Operation in BST (Simple-Logic ... Not Recursively)
+        public void InsertNode(int value)
+        {
+            // Create node and assign to it ...
+            temp = new Node();
+            temp.data = value;
+            temp.left = temp.right = null;
+            if (root == null)
+            {
+                root = temp;
+                return;
+            }
+            // Go to proper position to insert node ...
+            current = root;
+            while (current != null)
+            {
+                if (value == current.data)
+                {
+                    Console.WriteLine($"Value {value} is already exist");
+                    return;
+                }
+                if (value < current.data)
+                {
+                    parent = current;
+                    current = current.left;
+                }
+                else
+                {
+                    parent = current;
+                    current = current.right;
+                }
+            }
+            
+            if (value < parent.data)
+                parent.left = temp;
+            else
+                parent.right = temp;
+        }
+        // Searching Operation in BST (Simple-Logic ... Not Recursively)
+        public void SearchNode(int value)
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is Empty");
+                return;
+            }
+            else
+            {
+                // search value ...
+                current = root;
+                while (current != null)
+                {
+                    if (value == current.data)
+                    {
+                        Console.WriteLine($"Value {value} is found");
+                        return;
+                    }
+                    if (value < current.data)
+                        current = current.left;
+                    else
+                        current = current.right;
+                }
+            }
+            if (current == null)
+                Console.WriteLine("Value not found");
+        }
+    }
+}
---
+++ Program-BST-Insertion-Searching.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace BST_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BST bst = new BST();
+
+            Write("Enter Total Values : ");
+            int length = ToInt32(ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Write($"Enter Value {i + 1} : ");
+                int val = ToInt32(ReadLine());
+                bst.InsertNode(val);
+            }
+
+            Write("Enter value to search : ");
+            int v = ToInt32(ReadLine());
+            bst.SearchNode(v);
+
+
+            ReadKey();
+        }
+    }
+}
